96bdb741f66939d884676b2e32946c17a9ce486a
vaishnavi112 <51792745+vaishnavi112@users.noreply.github.com>
2019-06-25 20:14:16 +0530

    Add files via upload

---
+++ task 8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int num=0,i,mul;
+	printf("enter the number");
+	scanf("%d",&num);
+	printf("multiplication table is\n");
+		for(i=1;i<=10;i++)
+	{
+		printf("%d*%d=%d\n",num,i,num*i);
+		
+	}
+}
+	
+	
