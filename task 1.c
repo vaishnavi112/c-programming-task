857bbb3a0c6a6221a2e0bce1f09460a4777b4e9e
vaishnavi112 <51792745+vaishnavi112@users.noreply.github.com>
2019-06-25 20:11:22 +0530

    Add files via upload

---
+++ task 1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int age,phno;
+	char name;
+	printf("enter the name,age,phno of a person");
+	scanf("%s%d%d",&name,&age,&phno);
+	printf("name=%c,age=%d,phno=%d",name,age,phno);
+}
